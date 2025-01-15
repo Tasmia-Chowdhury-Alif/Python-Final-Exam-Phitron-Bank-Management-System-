a0b8ed48c6e2bd5f101d170577c39bab3a00b3db
Tasmia-Chowdhury-Alif <145268198+Tasmia-Chowdhury-Alif@users.noreply.github.com>
2025-01-15 20:56:08 +0600

    Update 1 bank.py
    i have forgotten to add an user login optin . so added user login

--- bank.py
+++ bank.py
@@ -9,8 +9,18 @@ class Bank :
         self.loan_feature = True
  
     def add_user(self, user):
-        user.account_no = len(self.users) + 1 
+        user.account_no = len(self.users) + 100 
         self.users.append(user)
+
+    def login_user(self, name, passward):
+        for i in self.users :
+            if(i.name == name and i.passward == passward):
+                return i 
 
     def add_admin(self, admin):
         self.admins.append(admin)
+
+    def login_admin(self, name, passward):
+        for i in self.admins :
+            if(i.name == name and i.get_passward == passward):
+                return i 
